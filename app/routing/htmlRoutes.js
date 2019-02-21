062582014c304e6d6aaf31c6f8cfc203e7fd648b
Joseph Cerankowski <joecerankowski@gmail.com>
2019-02-21 16:57:47 -0500

    make it better2

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -16,14 +16,14 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/","home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
 
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/","survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/","home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
