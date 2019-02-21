6005cfdb48c56be6ca803240b9ff1d668363effb
Joseph Cerankowski <joecerankowski@gmail.com>
2019-02-21 16:41:59 -0500

    make it better2

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friends = [
+  {
+      name: "Andrew",
+      photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3059318.png&w=350&h=254",
+      scores: [4, 5, 5, 3, 5, 3, 1, 5, 5, 4]
+  },
+  {
+      name: "Ben",
+      photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3907387.png&w=350&h=254",
+      scores: [1, 2, 5, 2, 1, 2, 5, 3, 1, 4]
+  },
+  {
+      name: "Josie",
+      photo: "https://www.wwwallaboutcats.com/wp-content/uploads/2016/10/shutterstock_454564069.jpg",
+      scores: [1, 2, 2, 5, 2, 5, 2, 1, 5, 2]
+  }
+];
+
+module.exports = friends
