54d590a033e5ccb9d9ecd1b0d3dea91ecf39fe73
Joseph Cerankowski <joecerankowski@gmail.com>
2019-02-22 13:21:25 -0500

    make it better

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -1,7 +1,8 @@
 // ===============================================================================
 // DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
 // ===============================================================================
-let path = require("path");
+var path = require("path");
 
 
 // ===============================================================================
@@ -9,7 +10,10 @@ let path = require("path");
 // ===============================================================================
 
 module.exports = function(app) {
-
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
 
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
