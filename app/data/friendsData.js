c4a2107c76c9d207ffc526cb096ce3b434c59b5b
Joseph Cerankowski <joecerankowski@gmail.com>
2019-02-22 00:38:06 -0500

    make it better

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -5,17 +5,17 @@
 var friendsArray = [
   {
     name: "Moth",
-    photo: "https://i.pinimg.com/originals/c1/66/67/c16667c38af6b32ff07db2c391466670.jpg",
+    photo: "https://",
     scores: [5,4,3,2,5,1,5,3,4,5]
   },
   {
     name: "Dreiyer",
-    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Minnie_Mouse.png/220px-Minnie_Mouse.png",
+    photo: "https://",
     scores: [5,2,3,4,4,1,2,3,4,1]
   },
   {
     name: "Pat McNulty",
-    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Minnie_Mouse.png/220px-Minnie_Mouse.png",
+    photo: "https://",
     scores: [1,2,5,3,3,2,4,1,1,1]
   },
  
