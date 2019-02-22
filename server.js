2638f0756f6cdf50f4235a8416cb36288588e3e9
Joseph Cerankowski <joecerankowski@gmail.com>
2019-02-21 21:32:07 -0500

    make it better2

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.use(express.static("app/public"));
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routing/apiRoutes.js")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
