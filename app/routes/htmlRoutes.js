4f64be33a861fce3d16878add1033a775d834c51
Joseph Cerankowski <joecerankowski@gmail.com>
2019-02-22 00:29:56 -0500

    make it better

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -1,6 +1,5 @@
 // ===============================================================================
 // DEPENDENCIES
-// include the path package to get the correct file path for html
 // ===============================================================================
 let path = require("path");
 
@@ -10,16 +9,9 @@ let path = require("path");
 // ===============================================================================
 
 module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
+
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-
-  app.get("/home", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 // ==============================================================================
 // DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 
 let express = require("express");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
 // ==============================================================================
 
 // Tells node that we are creating an "express" server
@@ -22,7 +20,6 @@ app.use(express.json());
 app.use(express.static("app/public"));
 // ================================================================================
 // ROUTER
-// The below points our server to a series of "route" files.
 // ================================================================================
 
 require("./app/routes/apiRoutes")(app);
@@ -30,7 +27,6 @@ require("./app/routes/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
-// The below code "starts" our server
 // =============================================================================
 
 app.listen(PORT, function() {
