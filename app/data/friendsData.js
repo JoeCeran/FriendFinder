aa2019a72e9b983ac4c0b24377981883a5b915c2
Joseph Cerankowski <joecerankowski@gmail.com>
2019-02-21 22:25:35 -0500

    make it better

---
+++ app/data/friendsData.js
@@ -0,0 +1,90 @@
+// ===============================================================================
+// DATA
+// Below data are possible friends to match
+// ===============================================================================
+
+let friendsArray = [
+  {
+    name: "Mickey Mouse",
+    photo: "https://i.pinimg.com/originals/c1/66/67/c16667c38af6b32ff07db2c391466670.jpg",
+    scores: [5,4,3,2,5,1,5,3,4,5]
+  },
+  {
+    name: "Minnie Mouse",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Minnie_Mouse.png/220px-Minnie_Mouse.png",
+    scores: [3,3,3,3,3,3,3,3,3,3]
+  },
+  {
+    name: "Donald Duck",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Donald_Duck.svg/220px-Donald_Duck.svg.png",
+    scores: [4,2,4,1,5,2,3,4,4,3]
+  },
+  {
+    name: "Snow White",
+    photo: "https://upload.wikimedia.org/wikipedia/en/e/e1/Snow_white_disney.png",
+    scores: [4,2,3,3,1,5,3,2,4,5]
+  },
+  {
+    name: "Kermit",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/6/62/Kermit_the_Frog.jpg/220px-Kermit_the_Frog.jpg",
+    scores: [4,1,2,3,1,2,4,3,1,5]
+  },
+  {
+    name: "Cinderella",
+    photo: "https://upload.wikimedia.org/wikipedia/en/e/e0/Cinderella_disney.png",
+    scores: [5,5,5,3,1,2,4,4,4,5]
+  },
+  {
+    name: "Pluto",
+    photo: "https://upload.wikimedia.org/wikipedia/en/6/6d/Plutodog.gif",
+    scores: [4,4,3,3,2,2,1,3,5,5]
+  },
+  {
+    name: "Elsa",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Elsa_from_Disney%27s_Frozen.png/220px-Elsa_from_Disney%27s_Frozen.png",
+    scores: [2,3,2,4,4,3,5,5,2,2]
+  },
+  {
+    name: "Aladdin",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Aladdin_Disney_pose.png/220px-Aladdin_Disney_pose.png",
+    scores: [3,4,2,4,5,4,3,4,1,3]
+  },
+  {
+    name: "Belle",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d5/Belle_disney.png/220px-Belle_disney.png",
+    scores: [4,3,5,5,2,1,4,2,3,4]
+  },
+  {
+    name: "Woody",
+    photo: "https://upload.wikimedia.org/wikipedia/en/0/01/Sheriff_Woody.png",
+    scores: [3,3,4,5,4,5,1,2,4,4]
+  },
+  {
+    name: "Shrek",
+    photo: "https://upload.wikimedia.org/wikipedia/en/4/4d/Shrek_%28character%29.png",
+    scores: [3,3,5,5,5,2,2,4,5,1]
+  },
+  {
+    name: "Fiona",
+    photo: "https://upload.wikimedia.org/wikipedia/en/b/b9/Princess_Fiona.png",
+    scores: [4,4,4,4,5,5,5,4,4,4]
+  },
+  {
+    name: "Buzz Lightyear",
+    photo: "https://upload.wikimedia.org/wikipedia/en/b/b4/Buzz_Lightyear.png",
+    scores: [1,1,4,4,4,2,2,3,3,3]
+  },
+  {
+    name: "Cruella De Vil",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/0/08/Cruella_de_Vil.gif/220px-Cruella_de_Vil.gif",
+    scores: [2,2,3,3,2,2,2,4,4,3]
+  },
+  {
+    name: "Ariel",
+    photo: "https://upload.wikimedia.org/wikipedia/en/7/77/Ariel_disney.png",
+    scores: [2,2,3,4,5,1,1,2,4,3]
+  }
+];
+
+// Export friendsData
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 // ==============================================================================
 // DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 
 let express = require("express");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
 // ==============================================================================
 
 // Tells node that we are creating an "express" server
@@ -17,15 +19,18 @@ let PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("app/public"));
 // ================================================================================
 // ROUTER
+// The below points our server to a series of "route" files.
 // ================================================================================
 
-//require("./app/routes/apiRoutes")(app);
+require("./app/routes/apiRoutes")(app);
 require("./app/routes/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
+// The below code "starts" our server
 // =============================================================================
 
 app.listen(PORT, function() {
