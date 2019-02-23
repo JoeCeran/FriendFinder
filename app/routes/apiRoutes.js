fbfa08401560eece3535116ce0900522b81a0cf1
Joseph Cerankowski <joecerankowski@gmail.com>
2019-02-23 08:43:52 -0500

    make it better

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -2,7 +2,7 @@
 // LOAD DATA
 // ===============================================================================
 
-let friendsData = require("../data/friendsData");
+let friends = require("../data/friends");
 
 
 // ===============================================================================
@@ -10,55 +10,37 @@ let friendsData = require("../data/friendsData");
 // ===============================================================================
 
 module.exports = function (app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function (req, res) {
-    res.json(friendsData);
+    res.json(friends);
   });
 
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  //When a user submits form data:
-  //JSON data is obtained from friendsData file
-  //For each friend in the JSON file, for each question, the scores for the friend in the JSON file
-  // are compared to the user's scores. 
-  //The 2 scores from each questions are subtracted from each other to get the difference.
-  //The differences from each question are added together to get a final score.
-  //the best match has the lowest score total.  The final score that is calculated from each
-  //friend/user is compared to each friend/user score to get the best match. 
-  // ---------------------------------------------------------------------------
-
   app.post("/api/friends", function (req, res) {
-    let newUser = req.body;
-    let bestMatch = {
+    var newUser = req.body;
+    var match = {
       name: "",
-      picture: ""
+      photo: ""
     }
-    let saveScore = 0;
-    for (let i = 0; i < friendsData.length; i++) {
+    var saveScore = 0;
+    for (var i = 0; i < friends.length; i++) {
       //set question to zero for each friend 
 
-      let questionScore = 0;
+      var questionScore = 0;
 
       for (let j = 0; j < newUser.scores.length; j++) {
-        questionScore += Math.abs(friendsData[i].scores[j] - newUser.scores[j]);
+        questionScore += Math.abs(friends[i].scores[j] - newUser.scores[j]);
       }
 
-      //first friend is the low score - so save it's data
-      //in the event of a tie, the last friend that is compared to is the best match
       if (i === 1) {
         saveScore = questionScore;
-        bestMatch.name = friendsData[i].name;
-        bestMatch.photo = friendsData[i].photo;
+        match.name = friends[i].name;
+        match.photo = friends[i].photo;
       }
       else {
         if (questionScore <= saveScore) {
           saveScore = questionScore;
-          bestMatch.name = friendsData[i].name;
-          bestMatch.photo = friendsData[i].photo;
+          match.name = friends[i].name;
+          match.photo = friends[i].photo;
         };
       };
     };
